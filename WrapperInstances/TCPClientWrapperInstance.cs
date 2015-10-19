0ffddf3907ff7de7cc397b53dfe542f6bae4631e
Aragas <micvitalij@gmail.com>
2015-10-19 19:47:25 +0300

    while is unsafe

--- WrapperInstances/TCPClientWrapperInstance.cs
+++ WrapperInstances/TCPClientWrapperInstance.cs
@@ -93,10 +93,7 @@ namespace PokeD.SCON.Windows.WrapperInstances
                     {
                         var numberOfBytesRead = Stream.Read(buffer, 0, buffer.Length);
                         if (numberOfBytesRead == 0)
-                            while (DataAvailable <= 0)
-                            {
-                                Thread.Sleep(1);
-                            }
+                            break;
 
                         receivedData.Write(buffer, 0, buffer.Length); //Write to memory stream
                         totalNumberOfBytesRead += numberOfBytesRead;
