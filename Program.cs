ccb8763202de56647d8b97df2aeeb19c66117c0b
Aragas <micvitalij@yandex.ru>
2016-10-02 17:24:28 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,40 +1,27 @@
 ﻿using System;
 using System.Net;
 
-using Aragas.Core.Wrappers;
-
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 using PokeD.Core.Extensions;
 
-using PokeD.SCON.Desktop.WrapperInstances;
-
 namespace PokeD.SCON.Desktop
 {
     public static class Program
     {
-        static Game Game { get; set; }
-
         static Program()
         {
-            AppDomainWrapper.Instance = new AppDomainWrapperInstance();
-            FileSystemWrapper.Instance = new FileSystemWrapperInstance();
-            TCPClientWrapper.Instance = new TCPClientFactoryInstance();
-            InputWrapper.Instance = new InputWrapperInstance();
-            ThreadWrapper.Instance = new ThreadWrapperInstance();
-
             PacketExtensions.Init();
         }
 
-        //[STAThread]
         public static void Main(params string[] args)
         {
             if (Type.GetType("Mono.Runtime") != null) // -- Running on Mono
                 ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
 
-            using (Game = new EmptyKeysUI(PlatformCode))
-                Game.Run();
+            using (var game = new EmptyKeysUI(PlatformCode))
+                game.Run();
             
         }
         private static void PlatformCode(Game client)
